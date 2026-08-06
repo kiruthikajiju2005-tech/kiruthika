0e189e9d69da785245d25ab0a9c03072643733bd
Kiruthika-02 <kiruthikajiju2005@gmail.com>
2026-08-06 12:01:52 +0530

    updated

--- largest.py
+++ largest.py
@@ -8,3 +8,12 @@ elif b>c:
 else:
     large=c
 print("Largest value is ",large)
+n=int(input("enter the value:"))
+nrev=0
+while(n!=0):
+    r=n%10
+    nrev=(nrev*10)+r
+    n//=10
+
+print("rverse digit=",nrev)    
+
